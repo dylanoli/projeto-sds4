198583863076bf3d4345815233448364638ee072
dylanoli <dylan.oli@homtail.com>
2021-09-10 21:09:03 -0300

    First request

--- backend/dsvendas/src/main/java/com/devsuperior/dsvendas/controller/SaleController.java
+++ backend/dsvendas/src/main/java/com/devsuperior/dsvendas/controller/SaleController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/sale")
+@RequestMapping(value = "/sales")
 public class SaleController {
     @Autowired
     private SaleService saleService;
